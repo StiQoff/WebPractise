efc1ecc766ee0c33f8c99971509a7d61ec60ba63
Sergey Gurkov <stiq.off@gmail.com>
2020-09-28 20:11:46 +0600

    Fourth lab

---
+++ localhostlpr4/rest08/controllers/BaseController.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\controllers;
+ 
+use yii\rest\Controller;
+use yii\filters\ContentNegotiator;
+use yii\web\Response;
+use yii\filters\auth\QueryParamAuth;
+
+class BaseController extends Controller{
+    public function behaviours(){
+        return [
+            'contentNegotiator' => [
+            'class' => ContentNegotiator::class,
+            'formats' => [
+            'application/json' =>
+            Response::FORMAT_JSON,
+            ],
+            ],
+            'authenticator' => [
+            'class' => QueryParamAuth::className(),
+            'tokenParam' => 'token',
+            ],
+            ];
+    }
+}
